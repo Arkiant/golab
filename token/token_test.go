9ffbffaec8c885772a0a31bfe456fbef5a0a6446
Arkiant <samuelporras@gmail.com>
2019-09-12 16:58:57 +0200

    Testing template

--- token/token_test.go
+++ token/token_test.go
@@ -2,32 +2,37 @@ package token
 
 import "testing"
 
+type Parameters struct {
+	Key   string
+	Value string
+}
+
+type SerializeStruct struct {
+	Name       string
+	Surname    string
+	Price      float32
+	Parameters []Parameters
+}
+
 func serializeTest(t *testing.T) {
 
-	type Parameters struct {
-		Key   string
-		Value string
-	}
-
-	type SerializeStruct struct {
-		Name       string
-		Surname    string
-		Price      float32
-		Parameters []Parameters
-	}
-
 	tc := []struct {
-		Name     string
-		ItemTest SerializeStruct
+		Name string
+		In   SerializeStruct
+		Out  string
 	}{
-		{"Serialize struct test", SerializeStruct{
-			Name:    "test",
-			Surname: "testSurname",
-			Price:   16.0,
-			Parameters: []Parameters{
-				{Key: "TestParameter", Value: "ValueParameter"},
+		{
+			"Serialize struct to string",
+			SerializeStruct{
+				Name:    "test",
+				Surname: "testSurname",
+				Price:   16.0,
+				Parameters: []Parameters{
+					{Key: "TestParameter", Value: "ValueParameter"},
+				},
 			},
-		}},
+			"test#testSurname#16.0#TestParameter[ValueParameter",
+		},
 	}
 
 	for _, tt := range tc {
@@ -39,5 +44,28 @@ func serializeTest(t *testing.T) {
 }
 
 func deserializeTest(t *testing.T) {
+	tc := []struct {
+		Name string
+		In   string
+		Out  SerializeStruct
+	}{
+		{
+			"Deserialize string to struct",
+			"test#testSurname#16.0#TestParameter[ValueParameter",
+			SerializeStruct{
+				Name:    "test",
+				Surname: "testSurname",
+				Price:   16.0,
+				Parameters: []Parameters{
+					{Key: "TestParameter", Value: "ValueParameter"},
+				},
+			},
+		},
+	}
 
+	for _, tt := range tc {
+		t.Run(tt.Name, func(t *testing.T) {
+
+		})
+	}
 }
