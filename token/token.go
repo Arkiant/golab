e8fe8542d94f364ec6d244c2c12e64c449a9b41d
Arkiant <samuelporras@gmail.com>
2019-09-13 10:56:28 +0200

    First serialize version

--- token/token.go
+++ token/token.go
@@ -1,1 +1,51 @@
 package token
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type Parameters struct {
+	Key   string
+	Value string
+}
+
+type SerializeStruct struct {
+	Price      float32
+	Name       string
+	Surname    string
+	Parameters []Parameters
+}
+
+func serialize(item SerializeStruct, sep, sepLevel2, paramSep string) string {
+	const (
+		name int = iota
+		surname
+		price
+		parameters
+	)
+
+	const (
+		paramKey int = iota
+		paramValue
+	)
+
+	b := bytes.Buffer{}
+	b.WriteString(item.Name)
+	b.WriteString(sep)
+	b.WriteString(item.Surname)
+	b.WriteString(sep)
+	b.WriteString(strconv.FormatFloat(float64(item.Price), 'f', 2, 32))
+	b.WriteString(sep)
+	for i, p := range item.Parameters {
+		b.WriteString(p.Key)
+		b.WriteString(sepLevel2)
+		b.WriteString(p.Value)
+		if i < len(item.Parameters)-1 {
+			b.WriteString(paramSep)
+		}
+	}
+
+	return b.String()
+
+}
--- token/token_test.go
+++ token/token_test.go
@@ -1,25 +1,22 @@
 package token
 
-import "testing"
+import (
+	"testing"
 
-type Parameters struct {
-	Key   string
-	Value string
-}
+	"github.com/stretchr/testify/assert"
+)
 
-type SerializeStruct struct {
-	Name       string
-	Surname    string
-	Price      float32
-	Parameters []Parameters
-}
+func TestSerialize(t *testing.T) {
 
-func serializeTest(t *testing.T) {
+	t.Parallel()
 
 	tc := []struct {
-		Name string
-		In   SerializeStruct
-		Out  string
+		Name      string
+		In        SerializeStruct
+		Out       string
+		Sep       string
+		SepLevel2 string
+		ParamSep  string
 	}{
 		{
 			"Serialize struct to string",
@@ -29,21 +26,53 @@ func serializeTest(t *testing.T) {
 				Price:   16.0,
 				Parameters: []Parameters{
 					{Key: "TestParameter", Value: "ValueParameter"},
+					{Key: "TestParameter2", Value: "ValueParameter2"},
+					{Key: "TestParameter3", Value: "ValueParameter3"},
 				},
 			},
-			"test#testSurname#16.0#TestParameter[ValueParameter",
+			"test#testSurname#16.00#TestParameter##ValueParameter###TestParameter2##ValueParameter2###TestParameter3##ValueParameter3",
+			"#",
+			"##",
+			"###",
 		},
 	}
 
 	for _, tt := range tc {
 		t.Run(tt.Name, func(t *testing.T) {
-
+			s := serialize(tt.In, tt.Sep, tt.SepLevel2, tt.ParamSep)
+			assert.Equal(t, tt.Out, s)
 		})
 	}
 
 }
 
-func deserializeTest(t *testing.T) {
+func BenchmarkSerialize(b *testing.B) {
+
+	s := SerializeStruct{
+		Name:    "test",
+		Surname: "testSurname",
+		Price:   16.0,
+		Parameters: []Parameters{
+			{Key: "TestParameter", Value: "ValueParameter"},
+			{Key: "TestParameter2", Value: "ValueParameter2"},
+			{Key: "TestParameter3", Value: "ValueParameter3"},
+			{Key: "TestParameter3", Value: "ValueParameter3"},
+			{Key: "TestParameter3", Value: "ValueParameter3"},
+			{Key: "TestParameter3", Value: "ValueParameter3"},
+			{Key: "TestParameter3", Value: "ValueParameter3"},
+		},
+	}
+
+	b.ResetTimer()
+	b.SetParallelism(4)
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		serialize(s, "#", "##", "###")
+	}
+}
+
+func TestDeserialize(t *testing.T) {
 	tc := []struct {
 		Name string
 		In   string
